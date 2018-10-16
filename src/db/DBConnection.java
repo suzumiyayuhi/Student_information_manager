69a7f2cc2f99e3b94a660c03afa107a1619d540f
Xinghai Xue <923051761@qq.com>
2018-10-16 15:04:56 +0800

    2018.10.16
    2018.10.16

--- src/db/DBConnection.java
+++ src/db/DBConnection.java
@@ -6,15 +6,18 @@ public class DBConnection {
 	//获取数据库连接
 	public static Connection getConn() {
 		Connection conn = null;		
-		String DRIVERNAME = "com.mysql.jdbc.Driver";		
-		String DBURL = "jdbc:mysql://localhost:3306/student_information_manager" +
-				"?useUnicode=true&characterEncoding=utf8";
+		String DRIVERNAME = "com.mysql.jdbc.Driver";
+//		String DRIVERNAME = "com.mysql.cj.jdbc.Driver";
+//		DRIVERNAME的注释版本用于mysql 8.0
+		String DBURL = "jdbc:mysql://localhost:3306/student_information_manager?useUnicode=true&characterEncoding=utf8";
+//		String DBURL = "jdbc:mysql://localhost:3306/student_information_manager?useUnicode=true&characterEncoding=utf8&serverTimezone=GMT%2B8&useSSL=false";
+//		DBURL的注释版本用于mysql 8.0
 		// 建立连接
 		try {
 			// 加载JDBC驱动
 			Class.forName(DRIVERNAME);
 			// 获取连接
-			conn = DriverManager.getConnection(DBURL, "root", "1996");			
+			conn = DriverManager.getConnection(DBURL, "root", "123456");
 		} catch (ClassNotFoundException e1) {
 			System.out.println(e1.getMessage());
 		} catch (SQLException e2) {
